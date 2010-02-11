486afb3a0d949447b31ff225ea85f294ef17d03e
rcarigna <rcarigna@gmail.com>
2010-02-11 13:36:43 -0500

    adding db_connect, header, index.php and style.css

---
+++ db_connect.php
@@ -0,0 +1,5 @@
+<?php
+	$db = mysqli_connect('localhost', 'alienuser', 'alien', 'aliendb')
+	or die ("ERROR: connecting to mysql server!");
+	
+?>
---
+++ header.php
@@ -0,0 +1,20 @@
+	<div id="header"><h1><a href="index.php">Awesome Music Watch</a></h1></div>
+	
+	<p><i>Index of Artists and Venues.</i></p>
+	
+	
+	<p>
+	
+	<div id="nav">
+	
+	<table border="5" witdth="760" >
+	<tr>
+	<td>Home</td>
+	<td>Add Band</td>
+	<td>Add Venue</td>
+	</tr>
+	</table>
+	
+	</div>
+
+	</p>
