5a63771d355b835f927074e0be615aa69a7ec35f
rcarigna <rcarigna@gmail.com>
2010-02-11 13:47:35 -0500

    attempting to add index.php and style.css

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Awesome Music Watch</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<div id="wrap">
+<body>
+
+<?php
+
+	include("header.php");
+	
+	echo '<div id=\'main\'>';
+
+	//include("db_connect.php");
+	//include("sidebar.php");
+	echo 'Welcome to the Awesome Music Watch Website!';
+	
+	
+	
+	echo '<div id = \'artist\'>';
+	
+	echo 'Artist Section';
+	
+	echo '</div>';
+	
+	echo '<div id = \'venue\'>';
+	echo 'Venue Section';
+	
+	echo '</div>';
+	
+	echo '</div>';
+
+?>
+
+
+</body>
+</div>
+</html>
