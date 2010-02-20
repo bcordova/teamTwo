3806f09c63a468b5984f5cd39519979f2742dd3f
unknown <Stephen@.(none)>
2010-02-20 12:44:12 -0500

    adding venu page + style

---
+++ searchVenue.php
@@ -0,0 +1,11 @@
+<form method = "post" action="venue.php">
+<label for="venueSearch">Search for a location</label>
+<input type="text" id="venueSearch" name="venueSearch" />
+<input type="submit" value="search" name="venue" />
+
+<label for="bandSearch">Search for a band at this location</label>
+<input type="text" id="bandSearch" name="venueSearch" />
+<input type="submit" value="search" name="band" />
+<?php
+
+?>
