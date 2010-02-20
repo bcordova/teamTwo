4d44eb6500bc0b8f3d57bcd86b4995a03c3183d0
rcarigna <rcarigna@gmail.com>
2010-02-20 12:37:55 -0500

    added featured venue to index.php

--- header.php
+++ header.php
@@ -9,15 +9,15 @@
 	
 	<table border="0" cellspacing="10" align="center">
 	<tr>
-	<td width="200">Home</td>
+	<!--<td width="200">Home</td>
 	<td width="200">Add Band</td>
-	<td width="200">Add Venue</td>
-	<!-- <td width="200"><?php print( '<a href="http://localhost/music/">Home</a>' ); ?></td>           
+	<td width="200">Add Venue</td>-->
+	<td width="200"><?php print( '<a href="http://localhost/music/">Home</a>' ); ?></td>           
 	
-	//Hyperlinks for header. URLs must be adjusting according to htdocs 
+	<!--//Hyperlinks for header. URLs must be adjusting according to htdocs -->
 
 	<td width="200"><?php print( '<a href="http://localhost/music/addband.php">Add Band</a>' ); ?></td>
-	<td width="200"><?php print( '<a href="http://localhost/music/">Add Venue</a>' ); ?></td> -->
+	<td width="200"><?php print( '<a href="http://localhost/music/">Add Venue</a>' ); ?></td> 
 	</tr>
 	</table>
 	
