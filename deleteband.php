a2909d8a2e6d13c486d66232caf5a2e7ca95e1db
Benjamin Cordova <bcordova@mail.umw.edu>
2010-02-22 16:46:39 -0500

    added delete band

---
+++ deleteband.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+ <HEAD>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Awesome Music Watch</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+
+ </HEAD>
+<div id="wrap">
+ <BODY>
+ <?php include("header.php"); ?>
+ <div id="main">
+
+ <?php
+
+  $bandName = $_GET['name'];
+?>
+<?php
+	include("db_connect.php");
+	
+	$query = "delete from band where name like '$bandName'";
+
+	$result = mysqli_query($db, $query)
+	  or die("Error querying Database");
+	
+  echo "<p>The band $bandName was successfully removed.</p>";
+  
+  
+  $query = "SELECT Name, Genre, website FROM band ORDER BY Year_Started";
+  
+  $result = mysqli_query($db, $query)
+   or die("Error Querying Database");
+  
+  echo "<table id=\"hor-minimalist-b\">\n<tr><th>Band Name</th><th>Genre</th><th>Website</th><tr>\n\n";
+  
+  while($row = mysqli_fetch_array($result)) {
+  	$name = $row['Name'];
+  	$genre = $row['Genre'];
+  	$url = $row['website'];
+  	echo "<tr><td  ><a href=\"bandview.php?name=$name\">$name</a></td><td>$genre</td><td ><a href=\"$url\">$url</a></td></tr>\n";
+  }
+ echo "</table>\n"; 
+  
+  mysqli_close($db);
+  
+  
+?>	
+
+
+</div>
+<?php include("sidebar.php"); ?>
+</div>
+ </BODY>
+</HTML>
